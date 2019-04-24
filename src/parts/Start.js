2a36c541babe442d510e2872d3cc16b270b5cafc
royweb <xytank@gmail.com>
2019-04-24 14:34:51 -0400

    First time using useState to hold values

--- src/parts/Start.js
+++ src/parts/Start.js
@@ -1,9 +1,23 @@
-import React from 'react';
+// useState enable hooks
+import React, { useState } from 'react';
 
 const start = props => {
+    // this is a function that replace state
+    // this has two elements
+    const inputValue = useState('');
+
+    const changeInput = (e) => {
+        // this will update the value of inputValue
+        inputValue[1](e.target.value);
+    };
+
     // top level sibling elements
     return <React.Fragment>
-        <input type="text" placeholder="Something..." />
+        <input
+            type="text"
+            placeholder="Something..."
+            onChange={ changeInput }
+            value={ inputValue[0] } />
         <button type="button">Enter</button>
     </React.Fragment>
 };
