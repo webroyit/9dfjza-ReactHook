711bb8d36f2fb6365a572c18d4f604e917185c04
royweb <xytank@gmail.com>
2019-05-02 12:15:36 -0400

    Example of converting class component into hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Header from './parts/Header';
 import Start from './parts/Start';
 import SingleState from './parts/SingleState';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Example of using React Hook</h1>
-        <Header />
-        <Start />
-        <SingleState />
-      </div>
-    );
-  }
+const app = props => {
+  return(
+    <div className="App">
+      <h1>Example of using React Hook</h1>
+      <Header />
+      <Start />
+      <SingleState />
+    </div>
+  );
 }
 
-export default App;
+export default app;
