2705aac94800dfb4fb7b7c88d63fc804e8dfc800
royweb <xytank@gmail.com>
2019-05-02 12:41:26 -0400

    Add logic to swtich page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 import Header from './parts/Header';
 import Start from './parts/Start';
 import SingleState from './parts/SingleState';
 
 const app = props => {
+  const [page, setPage] = useState('start');
+
+  // switch component
+  const changePage = pageName => {
+    setPage(pageName);
+  }
+
   return(
     <div className="App">
       <h1>Example of using React Hook</h1>
-      <Header />
-      <Start />
-      <SingleState />
+      <Header 
+        loadStart={changePage.bind(this, 'start')}
+        loadSingle={changePage.bind(this, 'single')} />
+      {page === 'start' ? <Start /> : <SingleState />}
     </div>
   );
 }
